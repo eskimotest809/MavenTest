df509663ee586e35c87ed2fd2f00bf992a84bb4a
eskimo <eskimo@A310>
2018-03-27 20:34:31 +0200

    Update new requirement 2

--- testmaven/src/main/java/Test/testmaven/App.java
+++ testmaven/src/main/java/Test/testmaven/App.java
@@ -6,8 +6,12 @@ package Test.testmaven;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        nmethod();
+    }
+    
+    public static void nmethod() {
+    	System.out.println("Yes it works.");
     }
 }
